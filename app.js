f1a0c311f8564b012c3984d8ef9914e69e1c67ff
shabitee <94993051+shabitee@users.noreply.github.com>
2022-12-22 17:24:36 +0000

    "final push"

--- app.js
+++ app.js
@@ -24,31 +24,6 @@ event.preventDefault();
 
 document.querySelector("#btn").addEventListener("click", dog)
 
-// let dataBox = document.querySelector("#content");
-
-// let art =(event) =>{
-
-//     const keyAPI = `https://dog.ceo/api/breeds/image/random`;
-
-//     fetch(keyAPI)
-
-//     .then((response) => response.json())
-
-//     .then((data) =>{
-
-//         console.log(data.data);
-
-//         dataBox.innerHTML = `<img src="${data.message}">`;
-
-//         })
-
-//     .catch((err) => console.error(err));
-
-// event.preventDefault();
-
-// }
-
-// document.querySelector("#btn").addEventListener("click", art);
 
 let databox2 = document.querySelector("#facts");
 
@@ -62,11 +37,11 @@ fetch('https://dog-facts-api.herokuapp.com/api/v1/resources/dogs?number=5')
 
         myJSON = JSON.stringify(data)
         
-        databox2.innerHTML = `<p>${myJSON}<p>`
+        databox2.innerHTML = `<p>${data[0].fact}<p>`
+        console.log(data)
 
     })
 
 })
 
     //let facts = document.querySelector()
-
